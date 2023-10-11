b0d011803aa567bad784912da3ef392bfeaabb05
saeed <saeedmr881@gmail.com>
2023-10-11 16:50:21 +0330

    add camera calibration

---
+++ camera_calibration/cpp_code/make_cpp/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/home/saeed/software/python/pose_estimation/camera_calibration/cpp_code/main.cpp.o"
+  "CMakeFiles/run.dir/home/saeed/software/python/pose_estimation/camera_calibration/cpp_code/main.cpp.o.d"
+  "run"
+  "run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
